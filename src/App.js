765a99ebd0fc1a49a175c6760a0a5585bbec34fc
Nitin Raj Singh <nitinrajsingh430@gmail.com>
2020-04-27 20:13:00 +0530

    box shadow generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [Horizontal, setHorizontal] = useState(10);
+  const [Vertical, setVertical] = useState(10);
+  const [Blur, setBlur] = useState(10);
+  const [Color, setColor] = useState("gray");
+  const [Switch, setSwitch] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="controls">
+        <label>Horizontal Length</label>
+        <input type="range" min="-200" max="200" value={Horizontal} onChange={(e)=>setHorizontal(e.target.value)}  />
+        <label>Vertical Length</label>
+        <input type="range" min="-200" max="200" value={Vertical} onChange={(e)=>setVertical(e.target.value)}  />
+        <label>Blur</label>
+        <input type="range" min="0" max="200" value={Blur} onChange={(e)=>setBlur(e.target.value)}  /><br/><br/>
+        <label className="blur">Blur</label>
+        <input type="color" value={Color} onChange={(e)=>setColor(e.target.value)}  /><br/><br/>
+        <div className="switch">
+          <label>
+            Outline
+            <input type="checkbox" value={Switch} onChange={()=>setSwitch(!Switch)} />
+            <span className="lever"></span>
+            Inset
+          </label>
+        </div>
+      </div>
+      <div className="output">
+        <div className="box" style={{boxShadow:`${Switch?"inset":""} ${Horizontal}px ${Vertical}px ${Blur}px ${Color}`}}>
+          <p>box-shadow:{Horizontal}px {Vertical}px {Blur}px {Color}</p>
+        </div>
+      </div>
     </div>
   );
 }
